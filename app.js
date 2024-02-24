167dde57b643e1d232f5e04fee2ad07fa1905a6c
Ovy Evbodi <evbodiovo@gmail.com>
2024-02-24 14:31:15 +0100

    Implement signin

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-console.log('we running!')
 import mysql2 from 'mysql2';
+import jsonwebtoken from 'jsonwebtoken';
+import express from 'express';
 
 // setup mysql connection
-
 // set database parameters
 const host = process.env.MYSQL_DB_HOST;
 const user = process.env.MYSQL_DB_USER;
 const password = process.env.MYSQL_DB_PASSWORD;
 const database = process.env.MYSQL_DB_DATABASE;
+
+// jsonwebtoken setup
+const secret = process.env.JWT_SECRET;
+
+// express parameters
+const port = process.env.PORT || 8080;
 
 const databaseDetails = {
     host,
@@ -15,16 +21,64 @@ const databaseDetails = {
     password,
     database
 };
+let dbConnection;
+var tok;
 
-const dbConnection = mysql2.createConnection(databaseDetails);
 
-//establish database connection
-try {
-    dbConnection.connect();
-    console.log(`connected to ${database} `)
-} catch (error) {
-    console.log(error)
-}
+// instantiate express app
+const app = express();
 
-// close db connection
-dbConnection.end();
+app.get('/', (req, res) => {
+    res.send('<h1>navigate to <a href=\'/signin\'>signin</a> or signup</h1>')
+})
+
+// signin
+app.get('/signin', (req, res) => {
+    const connectToDatabase = () => {
+        // creates a mysql database connection
+        dbConnection = mysql2.createConnection(databaseDetails);
+    
+        // establish database connection
+        try {
+            dbConnection.connect();
+            console.log(`connected to ${database}`)
+        } catch (error) {
+            console.log(error)
+        }
+    };
+
+    const verifyUser = (name, password) => {
+        // verifies a user
+        dbConnection.query(`SELECT * FROM users WHERE name='${name}' LIMIT 1;`, (error, results, fields) => {
+            if (error) {
+                console.log(error)
+            }
+            if (results.length != 1) {
+                console.log('no valid user found')
+            }
+            if (results[0].password == password) {
+                console.log(`password correct! Welcome ${name}`)
+                console.log(results)
+                const token = jsonwebtoken.sign({ name, password }, secret, { expiresIn: 120 });
+                res.type('json')
+                res.send({
+                    "jwt": token,
+                    "user": results[0]
+                })
+            } else {
+                console.log('incorrect password')
+            }
+        })
+    };
+
+    connectToDatabase()
+    verifyUser('chee', 'notsosecure')
+
+    // close db connection
+    dbConnection.end()
+
+})
+
+app.listen(port, () => {
+    console.log(`listening on port http://localhost:${port}`)
+})
